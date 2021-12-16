e2f284cebf444e0245f93ae1dce38518b74bfcd7
Fredrik Oseberg <fredrik.no@gmail.com>
2021-12-16 12:27:55 +0100

    fix: lower-ios version

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "UnleashProxyClientSwift",
     platforms: [
         .macOS(.v10_15),
-        .iOS(.v13),
+        .iOS(.v11),
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
