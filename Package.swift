0b8ec7b105ce75f9015d6ad50cf54e3f82050057
Alamos GmbH <46443521+alamos-gmbh@users.noreply.github.com>
2023-06-01 14:22:38 +0200

    Update Package.swift
    min iOS13

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "UnleashProxyClientSwift",
     platforms: [
         .macOS(.v10_15),
-        .iOS(.v12),
+        .iOS(.v13),
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
