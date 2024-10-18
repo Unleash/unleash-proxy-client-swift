cba5322693c082ba3bc8e3c72d0c9e1d68d0d070
benjamin-es-hall <b.e.s.hall@outlook.com>
2024-10-18 19:02:45 +0100

    feat: Boostrap jsonFile from path rather than bundle

--- Sources/UnleashProxyClientSwift/Client/Bootstrap.swift
+++ Sources/UnleashProxyClientSwift/Client/Bootstrap.swift
@@ -2,27 +2,21 @@ import Foundation
 
 public enum Bootstrap {
     case toggles([Toggle])
-    case jsonFile(String)
+    case jsonFile(path: String)
     
     var toggles: [Toggle] {
         switch self {
         case .toggles(let toggles):
             return toggles
             
-        case .jsonFile(let file):
-            guard
-                let filePath = Bundle.main.path(forResource: file, ofType: "json")
-            else {
-                fatalError("Bootstrap file does not exist: \(file).json")
-            }
-            
+        case .jsonFile(let path):
             do {
-                let data = try Data(contentsOf: URL(fileURLWithPath: filePath))
+                let data = try Data(contentsOf: URL(fileURLWithPath: path))
                 let decodedData: FeatureResponse = try JSONDecoder()
                     .decode(FeatureResponse.self, from: data)
                 return decodedData.toggles
             } catch {
-                fatalError("Failed to decode JSON file: \(file).json: \(error)")
+                fatalError("Failed to decode JSON file at path: \(path): \(error)")
             }
         }
     }
