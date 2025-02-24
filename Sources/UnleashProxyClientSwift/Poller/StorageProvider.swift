15bf1767ca6d490aeb6d9ff2f7b992f1b358a6aa
Aiden.lee <lsj8706@naver.com>
2025-02-24 18:29:03 +0900

    feat: store toggles at once when Poller stores values to StorageProvider

--- Sources/UnleashProxyClientSwift/Poller/StorageProvider.swift
+++ Sources/UnleashProxyClientSwift/Poller/StorageProvider.swift
@@ -1,5 +1,6 @@
 public protocol StorageProvider {
     func set(value: Toggle?, key: String)
+    func reset(keyedValues: [String: Toggle])
     func value(key: String) -> Toggle?
     func clear()
 }
