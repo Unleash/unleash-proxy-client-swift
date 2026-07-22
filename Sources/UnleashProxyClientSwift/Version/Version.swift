0720c3b6338f0aa62c20b921dea7a456ec374c90
irstavr <irini.stavrakantonaki@goeve.com>
2026-07-22 11:22:39 +0200

    chore: upgrade to v2.5.0

--- Sources/UnleashProxyClientSwift/Version/Version.swift
+++ Sources/UnleashProxyClientSwift/Version/Version.swift
@@ -1,3 +1,3 @@
 public struct LibraryInfo {
-    public static let version = "2.4.0"  // Update this string with each new release
+    public static let version = "2.5.0"  // Update this string with each new release
 }
